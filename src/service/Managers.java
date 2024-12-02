7b26e021bee109cb6bd1c11332482967c2fe1700
alexvladrak <avrak@yandex.ru>
2024-12-02 21:09:28 +0300

    Спринт 6, вариант 1

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -6,11 +6,11 @@ import interfaces.TaskManager;
 public class Managers {
 
     public Managers() {
-    };
+    }
 
     public static TaskManager getDefaulf() {
         return new InMemoryTaskManager();
-    };
+    }
 
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
