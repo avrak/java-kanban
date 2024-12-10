c943bdb8cbc8320bb397033861ee86e667e60184
alexvladrak <avrak@yandex.ru>
2024-12-10 20:19:18 +0300

    Спринт 7, вариант 1, плюс замечания от коммита 12489b2dbbe15a5449b30dbad44a7471e99f8419

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -2,16 +2,11 @@ package service;
 
 import interfaces.FileTaskManager;
 import interfaces.HistoryManager;
-import interfaces.TaskManager;
 
 public class Managers {
 
     public Managers() {
     }
-
-//    public static TaskManager getDefaulf() {
-//        return new InMemoryTaskManager();
-//    }
 
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
