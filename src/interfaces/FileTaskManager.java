77e7ec855376fa185d296808703251008e8c597f
alexvladrak <avrak@yandex.ru>
2024-12-10 20:05:50 +0300

    Спринт 7, вариант 1, плюс замечания от коммита 12489b2dbbe15a5449b30dbad44a7471e99f8419

--- src/interfaces/FileTaskManager.java
+++ src/interfaces/FileTaskManager.java
@@ -4,7 +4,10 @@ import model.ManagerReadException;
 import model.ManagerSaveException;
 
 public interface FileTaskManager {
+
     public void save() throws ManagerSaveException;
+
     public void restore(String fileName) throws ManagerReadException;
+
     public boolean checkFile(String fileName) throws ManagerReadException;
 }
